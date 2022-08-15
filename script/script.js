cb778b48172d77d1ba93b0aec274025cbd163ed8
Anthony Bussi <anthony.bussi@gmail.com>
2022-08-15 21:10:05 +0200

    Récupération des données de l'API

---
+++ script/script.js
@@ -0,0 +1,17 @@
+'use strict';
+
+let container = document.querySelector(".container");
+let quoteText = document.querySelector(".quote__text");
+let quoteAuthor = document.querySelector(".quote__author");
+let characterImg = document.querySelector(".character-img");
+
+
+fetch('https://thesimpsonsquoteapi.glitch.me/quotes')
+    .then(response => response.json())
+    .then(result => {        
+        let quote = result[0];
+
+        quoteText.innerHTML = quote.quote;
+        quoteAuthor.innerHTML = quote.character;
+        characterImg.src = quote.image;
+    });
